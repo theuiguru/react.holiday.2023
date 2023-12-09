2d738660360c0d977fc8bb812958debda75dc643
Chris Thomas <webmaster102@gmail.com>
2023-12-08 20:11:34 -0900

    ShowLess functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 //import './App.css'
 
-function App() {
+function ShowLess({ children }) {
   let [expanded, setExpanded] = React.useState(false);
   let [contentHeight, setContentHeight] = React.useState('none');
   const contentRef = React.useRef(null);
@@ -13,13 +13,21 @@ function App() {
   return (
     <div>
       <div ref={contentRef} style={{ overflow: 'hidden', maxHeight: expanded ? contentHeight : `100px`, transition: 'all .5s ease'}}>
-      <p>Most of you are familiar with the virtues of a programmer. There are three, of course: laziness, impatience, and hubris. (Larry Wall) Good code is its own best documentation. (Steve McConnell) It is practically impossible to teach good programming style to students that have had prior exposure to BASIC. As potential programmers, they are mentally mutilated beyond hope of regeneration. (E. W. Dijkstra) A good programmer is someone who always looks both ways before crossing a one-way street. (Doug Linder) C++ : Where friends have access to your private members. (Gavin Russell Baker)</p>
-
-      <p>19 Jan 2038 at 3:14:07 AM (End of the word according to Unix–2^32 seconds after January 1, 1970) Computer science education cannot make anybody an expert programmer any more than studying brushes and pigment can make somebody an expert painter. (Eric Raymond) It is practically impossible to teach good programming style to students that have had prior exposure to BASIC. As potential programmers, they are mentally mutilated beyond hope of regeneration. (E. W. Dijkstra) There are two ways to write error-free programs; only the third one works. (Alan J. Perlis)</p>
+        { children }
       </div>
       <button onClick={() => { setExpanded(!expanded); }}>Show {expanded ? 'less' : 'more'}</button>
     </div>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <>
+      <ShowLess>Hi there!</ShowLess>
+      <ShowLess>
+        <p>Most of you are familiar with the virtues of a programmer. There are three, of course: laziness, impatience, and hubris. (Larry Wall) Good code is its own best documentation. (Steve McConnell) It is practically impossible to teach good programming style to students that have had prior exposure to BASIC. As potential programmers, they are mentally mutilated beyond hope of regeneration. (E. W. Dijkstra) A good programmer is someone who always looks both ways before crossing a one-way street. (Doug Linder) C++ : Where friends have access to your private members. (Gavin Russell Baker)</p>
+        <p>19 Jan 2038 at 3:14:07 AM (End of the word according to Unix–2^32 seconds after January 1, 1970) Computer science education cannot make anybody an expert programmer any more than studying brushes and pigment can make somebody an expert painter. (Eric Raymond) It is practically impossible to teach good programming style to students that have had prior exposure to BASIC. As potential programmers, they are mentally mutilated beyond hope of regeneration. (E. W. Dijkstra) There are two ways to write error-free programs; only the third one works. (Alan J. Perlis)</p>
+      </ShowLess>
+    </>
+  )
+}
